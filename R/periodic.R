6a4258062dcd92318858e8264454e7894ad5c6b5
Unknown <eliocampitelli@gmail.com>
2018-08-27 23:22:13 -0300

    Adds examples
    closes #1

--- R/periodic.R
+++ R/periodic.R
@@ -10,7 +10,30 @@
 #' @details
 #'
 #' This is a generic function and ggperiodic provides methods for vectors, data frames
-#' and [ggplot2] Layers (geoms and stats).
+#' and ggplot2 Layers (geoms and stats).
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' x <- seq(0, 360 - 20, by = 20)
+#' df <- data.frame(x = x, y = cos(x*pi/180))
+#' df_p <- periodic(df, x = c(0, 360))
+#'
+#' ggplot(df_p, aes(x, y)) +
+#'    geom_line() +            # periodic data
+#'    geom_point(data = df) +  # non periodic data
+#'
+#' # Extend domain
+#' ggplot(df_p, aes(x, y), x = c(-180, 540)) +
+#'    geom_line() +
+#'    geom_point(data = df)
+#'
+#' # with non regular intervals
+#' x <- runif(30, 0, 360)
+#' df <- periodic(data.frame(x = x, y = cos(x*pi/180)),
+#'                x = c(0, 360))
+#' ggplot(df, aes(x, y), x = c(-180, 540)) +
+#'    geom_point()
 #'
 #' @export
 periodic <- function(object, ...) {
--- R/wrap.R
+++ R/wrap.R
@@ -2,9 +2,44 @@
 #'
 #' @param object a periodic data frame
 #' @param ... name-value pairs of expressions defining range specifications
-#' @param .group optional group column
+#' @param .group optional group column (see examples)
 #'
+#' @examples
 #'
+#' x <- seq(0, 360 - 20, by = 20)
+#' df <- data.frame(x = x, y = cos(x*pi/180))
+#' df_p <- periodic(df, x = c(0, 360))
+#'
+#' # wrap in default rante
+#' df_wrapped <- wrap(df_p)
+#' range(df_wrapped$x)
+#' range(df$x)
+#'
+#' # specify range
+#' df_wrapped <- wrap(df_p, x = c(-145, 365))
+#' range(df_wrapped$x)
+#'
+#' # with non regular intervals
+#' x <- runif(30, 0, 360)
+#' df <- periodic(data.frame(x = x, y = cos(x*pi/180)),
+#'                x = c(0, 360))
+#' df_wrapped <- wrap(df, x = c(-180, 540))
+#' range(df_wrapped$x)
+#' range(df$x)
+#'
+#' # This example illustrates the use of the .group parameter
+#' library(ggplot2)
+#' map <- periodic(map_data("world"), long = long)
+#'
+#' # If wrapped without .group, the repated parts of the map
+#' # have the same group and so polygons are not correctly defined.
+#' ggplot(map_wrapped, aes(long, lat, group = group)) +
+#'     geom_path()
+#'
+#' # Using groups, you get the correct grouping.
+#' map_wrapped <- wrap(map, long = c(-180, 360), .group = group)
+#' ggplot(map_wrapped, aes(long, lat, group = group)) +
+#'     geom_path()
 #'
 #' @export
 wrap <- function(object, ...) {
